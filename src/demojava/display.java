98970b6e36a90a86c8c2b960a3ff17fa279891fd
IrukullaSravya <irukullasravya31@gmail.com>
2022-07-10 19:48:05 +0530

    newchange
    Signed-off-by: IrukullaSravya <irukullasravya31@gmail.com>

--- src/demojava/display.java
+++ src/demojava/display.java
@@ -4,7 +4,7 @@ public class display {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("welcome");
+		System.out.println("welcome eclipse");
 		System.out.print(1+2);
 	}
 

16e9fb448dbb59a4b9dfe262a00aa06f612d9baa
IrukullaSravya <108988916+IrukullaSravya@users.noreply.github.com>
2022-07-17 22:53:21 +0530

    updatedjavaline

--- src/demojava/newdemo.java
+++ src/demojava/newdemo.java
@@ -4,7 +4,7 @@ public class newdemo {
 
 	public static void main(String[] args) {
 		System.out.print("eclipse");
-		System.out.print("New demo for practice");
+		System.out.print("New demo for practice for fetch");
 
 
 	}
